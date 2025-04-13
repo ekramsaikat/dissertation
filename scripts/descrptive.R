57b84200ecb57be82f7ae008048269d863deef60
ekramsaikat <saikathossian024@gmail.com>
2025-04-13 18:07:05 +0600

    6th commit

--- scripts/descrptive.R
+++ scripts/descrptive.R
@@ -9,6 +9,7 @@ install.packages("report")
 install.packages("usethis")
 install.packages("broom.helpers")
 install.packages("tidyverse")
+
 #load packages
 
 library(broom.helpers)
@@ -31,11 +32,16 @@ library(scales)
 #read data
 Data<-readxl::read_excel("coded data/WASH Data.xlsx",sheet = 3)
 Data2<-readxl::read_excel("coded data/WASH Data.xlsx",sheet = 1)
-View(Data)
+WASH_FIT_data<-readxl::read_excel("coded data/WASH Data.xlsx",sheet = 5)
+
+
+
 
 #check missing values
 sum(is.na(Data))
 miss_var_summary(Data)
+
+
 
 #categorize of age
 
@@ -52,51 +58,145 @@ Data<- Data|>
     `Household Monthly Income (BDT)`<12500 ~"Lower",
     `Household Monthly Income (BDT)`>=12500&`Household Monthly Income (BDT)`<=21500~"Middle class",
     `Household Monthly Income (BDT)`>21500~"Upper class"
+  
   ))
+
+
+
+
 #export processed data as csv format
 
 write.csv(Data,"D:/dissertation/dissertation/coded data.csv")
 Data<-library(readxl)
 
+
+
+
 #import processed data as excel format
 Data<-readxl::read_excel("D:/dissertation/dissertation/coded data.xlsx")
 
+
+
+
+#joined dataset
+Data<-right_join(Data,WASH_FIT_data, by=c("Name of Upazila Health Complex"))
+View(Data)
+
+
+
+
 # descriptive table
-Data|>
-  select(1:15)|>
+
+dplyr::select(Data,2:15)|>
   tbl_summary(
     statistic = list(
-      all_continuous()~"{mean}±{sd}"),
+      all_continuous()~"{mean}({sd})"
+    ),
     type = c(13,14,15)~"categorical"
   )|>
   as_gt()|>
   gtsave("D:/dissertation/dissertation/tables/1.decriptive table.docx")
 
+
+
+
+
+
+#lin regression
+
+
 #socio-demographic factors influencing perception score(uvregression)
-Data|>
-  select(1:15,`Total perception score`)|>
+dplyr::select(Data,2:15,'Total perception score')|>
   tbl_uvregression(
     method = lm,
     y=`Total perception score`
     )|>
   bold_p(t=0.05)|>
   as_gt()|>
-  gtsave("D:/dissertation/dissertation/tables/2.factors influencing total perception score.docx")
+  gtsave("D:/dissertation/dissertation/tables/2.1factors influencing total perception score.docx")
+
 
 #socio-demographic factors influencing total PSQ score(uvregression)
 
-Data|>
-  select(1:15,`Total PSQ score`)|>
+
+dplyr::select(Data,2:15,"Total PSQ score")|>
   tbl_uvregression(
     method = lm,
     y=`Total PSQ score`
   )|>
   bold_p(t=0.05)|>
   as_gt()|>
-  gtsave("D:/dissertation/dissertation/tables/3.factors influencing total PSQ score.docx")
+  gtsave("D:/dissertation/dissertation/tables/2.2factors influencing total PSQ score.docx")
+
+
+
+
+
+#categorization of total perception score
+median(Data$`Total perception score`)
+
+Data<- Data|>
+  mutate(level_of_perception=case_when(
+    `Total perception score`<51~"Poor",
+    
+    `Total perception score`>=51~"Good"
+  ))
+
+
+#categorization of total PSQ score
+
+
+Data<-Data|>
+  mutate(level_of_satisfaction=case_when(
+    `Total PSQ score`<42 & `Total PSQ score`>=18~"poor",
+    `Total PSQ score`>=42 &`Total PSQ score`<=65~"medium",
+    `Total PSQ score`>=66 & `Total PSQ score`<=90~"good"
+  ))
+
+
+
+
+
+#factors influencing level of perception(binary log regression )
+
+Data$level_of_perception<-as.factor(Data$level_of_perception)
+
+
+dplyr::select(Data,2:15,'level_of_perception')|>
+  tbl_uvregression(
+    method = glm,
+    y=level_of_perception,
+    method.args = list(family=binomial)
+    
+  )|>bold_p(t=0.05)|>as_gt()|>
+  gtsave("D:/dissertation/dissertation/tables/2.3 binary logistic reg of level of perception.docx")
+
+
+
+
+#factors influencing level of satisfaction (ordinal log regression )
+
+Data$level_of_satisfaction<-as.factor(Data$level_of_satisfaction)
+
+dplyr::select(Data,2:15,'level_of_satisfaction')|>
+  tbl_uvregression(
+    method = glm,
+    y=level_of_satisfaction,
+    method.args = list(family=binomial)
+    
+  )|>bold_p(t=0.05)|>as_gt()
+
+
+
 
 
 #difference table
+
+
+
+
+
+
 
 Data|>
   select(`Total PSQ score`,`Total perception score`,Gender)|>
@@ -109,40 +209,45 @@ Data|>
   as_gt()|>
   gtsave("D:/dissertation/dissertation/tables/4.Genderwise difference on total PSQ score and total perception score.docx")
  
- #Association table between total perception score and total psq score(correalation)
-cor(Data$`Total perception score`,Data$`Total PSQ score` )
-
-Data|>
-  select(`Total PSQ score`,`Total perception score`)|>
-  correlation()|>
-  summary(redundant=T)|>
-plot()
- 
-glimpse(Data)
-
-
-#categorization of total perception score
- median(Data$`Total perception score`)
-
- Data<- Data|>
-   mutate(leve_of_perception=case_when(
-    `Total perception score`<51~"Poor",
-     
-     `Total perception score`>=51~"Good"
-   ))
  
 
 
- #categorization of total PSQ score
+
+
+
+#association table
+
+
+
+#correlation of all continious variable
+
+dplyr::select(Data,'Total PSQ score','Total perception score',54:59)|>
+  correlation()|>summary(redundant=T)|>
+  plot()
+ 
+
+
+#
+ 
+ #Association table between level of perception and gender
+ 
+ 
+ #Association table between level of perception and Education
+ 
+ 
+ #Association table between level of perception and income
+ 
+ 
+ 
+ #Association table between level of satisfaction and gender
+ 
+ 
+ #Association table between level of satisfaction and Education
+ 
+ 
+ #Association table between level of satisfaction and income
 
  
- Data<-Data|>
-   mutate(level_of_satisfaction=case_when(
-     `Total PSQ score`<42 & `Total PSQ score`>=18~"poor",
-     `Total PSQ score`>=42 &`Total PSQ score`<=65~"medium",
-     `Total PSQ score`>=66 & `Total PSQ score`<=90~"good"
-   ))
-
 #Association table between level of perception and level of satisfaction
  Data|>
   select(Gender,leve_of_perception,level_of_satisfaction)|>
@@ -153,22 +258,13 @@ glimpse(Data)
    gtsave("D:/dissertation/dissertation/tables/5.Genderwise association on level of perception and level of satisfaction.docx")
  
 
-#factors influencing level of perception(binary log regression )
-Data$leve_of_perception<-as.factor(Data$leve_of_perception)
+
+
+
+
+
+ #figure
  
- Data|>
-   select(1:15,leve_of_perception)|>
-   tbl_uvregression(
-     method = glm,
-     y=leve_of_perception,
-     method.args = list(family=binomial)
-    
-   )|>bold_p(t=0.05)|>as_gt()|>
-   gtsave("D:/dissertation/dissertation/tables/6.docx")
-   
-
-
-#factors influencing total PSQ score(mvregression)
 
 #figure1. distribution of perception regarding WASH facilities
  
@@ -195,6 +291,10 @@ long_fig_data<-long_fig_data|>filter(!is.na(Response))
 
 
 summary_data$Response<-factor(summary_data$Response,levels = c("Strongly disagree","Disagree","Strongly Agree", "Agree", "Neutral"))
+
+
+
+
 
 plot1<-ggplot(summary_data,aes(x=Question,y=Percentage,fill = Response))+
   geom_bar(stat = "identity",position = "fill")+
@@ -234,6 +334,10 @@ summary_data2<-long_fig_data2|>
 
 summary_data2$Response<-factor(summary_data2$Response,levels = c("Strongly disagree","Disagree","Strongly Agree", "Agree", "Neutral"))
  
+
+
+
+
  plot2<-ggplot(summary_data2,aes(x=Question,y=Percentage,fill = Response))+
    geom_bar(stat = "identity",position = "fill")+
    coord_flip()+
